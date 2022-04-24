3632e484a016a980775f6fff5e40ffce3dde6a09
infosergio2020 <infosergio2020@gmail.com>
2022-04-23 22:53:26 -0300

    carrousel agregado en sedes

--- static/js/index.js
+++ static/js/index.js
@@ -6,8 +6,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     createParallax(image);
 });
 
-
-
 // funcion encargada de aplicar estilos para esconder el navbar cuando se hace scrollY (nav--hidden)
 function navbarHideEffect(nav,clase) {
     let lastScrollY = window.scrollY; // te da un numero de 0 a n que simboliza el nro de pixels que se desplaza desde el origen
@@ -26,3 +24,26 @@ function createParallax(item) {
 function toggleCard(id,cClas) {
     document.getElementById(id).classList.toggle(cClas);
 }
+
+// mapa
+// Initialize and add the map
+function initMap() {
+    // [START maps_add_map_instantiate_map]
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // [END maps_add_map_instantiate_map]
+    // [START maps_add_map_instantiate_marker]
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+    // [END maps_add_map_instantiate_marker]
+  }
+  
+  window.initMap = initMap;
