ee9c1fda0e16688add70398109ad04f5b5b63e67
infosergio2020 <infosergio2020@gmail.com>
2022-04-25 22:22:41 -0300

    se agrego mapa de leaftlet a sedes y horarios

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const nav = document.querySelector("nav");
     navbarHideEffect(nav,"nav--hidden");
     createParallax(image);
+    
+    initMap();
 });
 
 // funcion encargada de aplicar estilos para esconder el navbar cuando se hace scrollY (nav--hidden)
@@ -23,4 +25,19 @@ function createParallax(item) {
 
 function toggleCard(id,cClas) {
     document.getElementById(id).classList.toggle(cClas);
+}
+
+// funcion encargada de inicializar el mapa
+function initMap() {
+    
+	var map = L.map('map').setView([51.505, -0.09], 13);
+
+	var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(map);
 }
